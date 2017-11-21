7c8424d0d039c173fd83e23e03c1a7693eb36ff7
Eyad Alshafi <eyad.alshafi@gmail.com>
2017-11-21 05:32:26 -0600

    version 1.0

---
+++ script.js
@@ -0,0 +1,19 @@
+function randomQuote() {
+  $.ajax({
+      url: "https://api.forismatic.com/api/1.0/?",
+      dataType: "jsonp",
+      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+      success: function( response ) {
+        $("#random_quote").html("<p id='random_quote' class='text'>" +
+          response.quoteText + "<p class='author'>&dash;" + response.quoteAuthor + "</p>");
+        $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
+          ' (' + response.quoteAuthor + ')');
+      }
+  });
+}
+
+$(function() {
+  randomQuote();
+  $('#update').on('click', randomQuote);
+});
+
